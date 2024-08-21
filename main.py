201a51dc3361de2d030fd085f1473831bff8da7c
Nossila-usb <minimonik1909@gmail.com>
2024-08-21 15:30:21 -0300

    versao 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+# funcao lambda
+somar = lambda x,y: x + y
+
+
+
+
+# programa principal
+x = int(input('Informe o valor do x: '))
+y = int(input('Informe o valor do y: '))
+
+# exibe o resultado
+print(f'A soma de {x} e {y} e {somar(x, y)}.')
